a8bb0b4d62ca0d0a0f68a160da7195275aa0cbe1
Diego Portella (c) <dportella@omnissa.com>
2026-07-10 11:16:15 -0300

    feat: create folder structure, set up environments and configure HttpClient

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  tmdbApiKey: 'INSERT_YOUR_API_KEY_HERE',
+  tmdbAccessToken: 'INSERT_YOUR_ACCESS_TOKEN_HERE',
+  apiUrl: 'https://api.themoviedb.org/3'
+};
