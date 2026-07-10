4e4bc25c7e0b8d413acc1e4079dd6ad38a064727
Diego Portella (c) <dportella@omnissa.com>
2026-07-10 20:38:17 -0300

    feat: add pagination to movie list

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -12,28 +12,49 @@ import { RouterLink } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
   private movieService = inject(MovieService);
+  
   public movies = signal<Movie[]>([]);
+  public currentPage = signal<number>(1);
+  public currentQuery = signal<string>('');
 
   ngOnInit(): void {
-    this.loadPopularMovies();
+    this.loadMovies();
   }
 
-  loadPopularMovies(): void {
-    this.movieService.getPopularMovies().subscribe({
-      next: (response: any) => this.movies.set(response.results),
-      error: (err: any) => console.error('Erro ao buscar populares:', err)
-    });
+  loadMovies(): void {
+    const query = this.currentQuery();
+    const page = this.currentPage();
+
+    if (!query) {
+      this.movieService.getPopularMovies(page).subscribe({
+        next: (response: any) => this.movies.set(response.results),
+        error: (err: any) => console.error('Erro ao buscar populares:', err)
+      });
+    } else {
+      this.movieService.searchMovies(query, page).subscribe({
+        next: (response: any) => this.movies.set(response.results),
+        error: (err: any) => console.error('Erro ao buscar filmes:', err)
+      });
+    }
   }
 
   onSearch(query: string): void {
-    if (!query.trim()) {
-      this.loadPopularMovies();
-      return;
-    }
+    this.currentQuery.set(query.trim());
+    this.currentPage.set(1);
+    this.loadMovies();
+  }
 
-    this.movieService.searchMovies(query).subscribe({
-      next: (response: any) => this.movies.set(response.results),
-      error: (err: any) => console.error('Erro ao buscar filmes:', err)
-    });
+  nextPage(): void {
+    this.currentPage.update(page => page + 1);
+    this.loadMovies();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  previousPage(): void {
+    if (this.currentPage() > 1) {
+      this.currentPage.update(page => page - 1);
+      this.loadMovies();
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
   }
 }
