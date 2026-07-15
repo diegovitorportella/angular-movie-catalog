30a746a5b04a930a659ba85de7de888530c3aec9
Diego Portella (c) <dportella@omnissa.com>
2026-07-15 10:05:02 -0300

    feat: implement 404 not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,15 @@ import { HomeComponent } from './components/home/home';
 import { MovieDetailsComponent } from './components/movie-details/movie-details';
 import { FavoritesComponent } from './components/favorites/favorites';
 import { authGuard } from './guards/auth-guard';
+import { NotFound } from './components/not-found/not-found';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'movie/:id', component: MovieDetailsComponent },
-  
   { 
     path: 'favorites', 
     component: FavoritesComponent, 
     canActivate: [authGuard] 
   },
+  { path: '**', component: NotFound }
 ];
